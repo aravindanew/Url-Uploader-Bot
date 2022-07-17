feb4f08456afadcf2f635e7aefe9797fe9c65092
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <93422036+MrAbhi2k3@users.noreply.github.com>
2022-07-17 21:47:20 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -12,7 +12,8 @@ from translation import Translation
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
-
+from plugins.Settings.settings import OpenSettings
+from database.access import clinton
 
 @Clinton.on_callback_query()
 async def button(bot, update):
@@ -34,15 +35,46 @@ async def button(bot, update):
             reply_markup=Translation.ABOUT_BUTTONS,
             disable_web_page_preview=True
         )
+    elif update.data == "OpenSettings":
+        await update.answer()
+        await OpenSettings(update.message)
+    elif update.data == "showThumbnail":
+        thumbnail = await clinton.get_thumbnail(update.from_user.id)
+        if not thumbnail:
+            await update.answer("You didn't set any custom thumbnail!", show_alert=True)
+        else:
+            await update.answer()
+            await bot.send_photo(update.message.chat.id, thumbnail, "Custom Thumbnail",
+                               reply_markup=types.InlineKeyboardMarkup([[
+                                   types.InlineKeyboardButton("Delete Thumbnail",
+                                                              callback_data="deleteThumbnail")
+                               ]]))
+    elif update.data == "deleteThumbnail":
+        await clinton.set_thumbnail(update.from_user.id, None)
+        await update.answer(Translation.DEL_ETED_CUSTOM_THUMB_NAIL, show_alert=True)
+        await update.message.delete(True)
+    elif update.data == "setThumbnail":
+        await update.message.edit_text(
+            text=Translation.SETTINGS_TEXT,
+            reply_markup=Translation.SETTINGS_BUTTONS,
+            disable_web_page_preview=True
+        )
+
+    elif update.data == "triggerUploadMode":
+        await update.answer()
+        upload_as_doc = await clinton.get_upload_as_doc(update.from_user.id)
+        if upload_as_doc:
+            await clinton.set_upload_as_doc(update.from_user.id, False)
+        else:
+            await clinton.set_upload_as_doc(update.from_user.id, True)
+        await OpenSettings(update.message)
+    elif "close" in update.data:
+        await update.message.delete(True)
+
+    elif "|" in update.data:
+        await youtube_dl_call_back(bot, update)
+    elif "=" in update.data:
+        await ddl_call_back(bot, update)
 
     else:
         await update.message.delete()
-
-@Clinton.on_callback_query()
-async def button(bot, update):
-
-    cb_data = update.data
-    if "|" in cb_data:
-        await youtube_dl_call_back(bot, update)
-    elif "=" in cb_data:
-        await ddl_call_back(bot, update)
