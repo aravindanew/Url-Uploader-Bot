ab2785bc24a50fcbc1b7be04a64f02b5389d1f17
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <93422036+MrAbhi2k3@users.noreply.github.com>
2022-08-05 10:18:53 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -11,7 +11,9 @@ import requests, urllib.parse, filetype, os, time, shutil, tldextract, asyncio, 
 from PIL import Image
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
-from sample_config import Config
+    from sample_config import Config
+else:
+    from config import Config
 from pyrogram import filters
 from database.access import clinton
 from translation import Translation
